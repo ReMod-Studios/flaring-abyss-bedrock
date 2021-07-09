108d4c8398a556044e14bd0b0acad30fd7bcb790
LePichu <ishat.prsnl65844@gmail.com>
2021-07-09 21:40:21 +0530

    ficks packager bcoz im dum

--- Packager.bat
+++ Packager.bat
@@ -4,8 +4,8 @@ set _my_datetime=%_my_datetime::=%
 set _my_datetime=%_my_datetime:/=_%
 set _my_datetime=%_my_datetime:.=_%
 
-jar -cfM "flabyss-bev.mcpack" "Flabyss [Bev.]"
-jar -cfM "flabyss-res.mcpack" "Flabyss [Res.]"
+jar -cfM "flabyss-bev.mcpack" "Flaring Abyss [Bev.]"
+jar -cfM "flabyss-res.mcpack" "Flaring Abyss [Res.]"
 jar -cfM "flabyss-test-%_my_datetime%.mcaddon" "flabyss-bev.mcpack" "flabyss-res.mcpack" 
 
 del "flabyss-bev.mcpack"
